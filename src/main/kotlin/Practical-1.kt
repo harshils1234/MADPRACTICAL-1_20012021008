8f8e37ef85f1a1490ff322cc964fbc4f22d71699
harshils1234 <harshilsidapara@20gnu.ac.in>
2022-08-07 13:28:27 +0530

    Practical-1

---
+++ src/main/kotlin/Practical-1.kt
@@ -0,0 +1,22 @@
+fun main()
+{
+    var a :Int=22
+    println("Integer Value:$a")
+    var b = 1.5
+    println("Float Value:$b")
+    var c :Char= 'T'
+    println("Character Value:$c")
+    var d :String="HMS"
+    println("String Value:$d")
+    var e :Boolean=false
+    println("Boolean Value:$e")
+    var f :Double=93.36
+    println("Double Value:$f")
+    var g :Long=338927847
+    println("Long Value:$g")
+    var h:Short=-2
+    println("Short Value:$h")
+    var i:Byte=127
+    println("Byte Value:$i")
+
+}
