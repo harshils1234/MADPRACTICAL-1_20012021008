b898c17c5d4425d24638ef31ec24fe49311e8031
harshils1234 <harshilsidapara@20gnu.ac.in>
2022-08-07 14:32:57 +0530

    Practical-3

---
+++ src/main/kotlin/practical3.kt
@@ -0,0 +1,32 @@
+fun main()
+{
+    print("Student Enrollment No.:")
+    var x = readLine()!!.toLong()
+    print("Student Name :")
+    var y= readLine()
+    print("Student Branch :")
+    var a= readLine()
+    print("Student Class :")
+    var b= readLine()
+    print("Student Batch :")
+    var c= readLine()
+    print("Student College Name:")
+    var d= readLine()
+    print("Student University Name :")
+    var e= readLine()
+    print("Student age :")
+    var z= readLine()!!.toInt()
+
+    println("******************************************")
+
+    println("Enrollment No.:$x")
+    println("Name:$y")
+    println("Branch:$a")
+    println("Class:$b")
+    println("Batch:$c")
+    println("College Name:$d")
+    println("University Name:$e")
+    println("Age.:$z")
+
+
+}
