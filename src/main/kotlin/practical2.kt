66ee7f2f3ac7bb3396ff38f8ce2a7fdcc5b1765b
harshils1234 <harshilsidapara@20gnu.ac.in>
2022-08-07 14:09:13 +0530

    Practical-2

---
+++ src/main/kotlin/practical2.kt
@@ -0,0 +1,15 @@
+fun main()
+{
+    print("Integer Value:")
+    var x = readLine()!!.toDouble()
+    println("Double value(From Integer) :$x")
+
+    print("string Value :")
+    var y = readLine()!!.toInt()
+    println("Integer value(From String):$y")
+
+    print("string Value :")
+    var z= readLine()!!.toDouble()
+    println("Double Value(From string):$z")
+
+}
